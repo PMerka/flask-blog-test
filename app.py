06d8df5c6cfea15ddabca1259d5b13cc868dbf49
merkap <pavel@merka.eu>
2021-08-27 22:05:32 +0200

    404 page added

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ list=["apple", "orange", "banana"]
 def index(name=None):
     return render_template('index.html', name=name, list=list)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
 
 app.run(debug=True)
